6c66d6314099fd27d46f138ddbaf547ff5316c54
romee97 <rkloniecki@gmail.com>
2024-11-14 02:39:02 +0100

    model

---
+++ ToDoList.Model/ToDoTask.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Model
+{
+    public class ToDoTask
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime TaskDate { get; set; }
+        public bool IsDone { get; set; }
+        public int Id { get; set; }
+    }
+}
